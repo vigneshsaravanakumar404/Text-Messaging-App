91c573cfc814eca992c071f281ac0e845fa1d674
vigneshsaravanakumar404 <10021913@sbstudents.ir>
2023-05-09 10:33:15 -0400

    5/9/2023

--- app/src/main/java/com/example/textmessagingapp/MainActivity.java
+++ app/src/main/java/com/example/textmessagingapp/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.textmessagingapp;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +18,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Broadcast Listener for SMS
+        BroadcastReceiver broadcastReceiver = new smsReceiver();
+        IntentFilter intentFilter = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
+        registerReceiver(broadcastReceiver, intentFilter);
 
-        
+    }
+
+    public static class smsReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "SMS Received", Toast.LENGTH_SHORT).show();
+            Log.d("SMS", "SMS Received");
+        }
     }
 }
+
+// TODO:
+// Create Interface
+// Send messages
+// Receive messages
+// AI
+// Icon
+// App Name
+// Save Messages
+// Different Chats for each number
