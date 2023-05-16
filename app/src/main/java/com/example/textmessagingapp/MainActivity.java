9a36267bcd5bb55bbb9e83082b08c63e14810d86
vigneshsaravanakumar404 <10021913@sbstudents.ir>
2023-05-16 11:47:34 -0400

    Stable API Key Commit

--- app/src/main/java/com/example/textmessagingapp/MainActivity.java
+++ app/src/main/java/com/example/textmessagingapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.telephony.SmsManager;
@@ -19,15 +20,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
 
     private static final int SMS_PERMISSION_REQUEST_CODE = 1;
-    final String phoneNumber = "+15555215554";
-    final String API_KEY = APIKey.API_KEY;
+    final String phoneNumber = "+15555215556";
     BroadcastReceiver br;
     Handler handler = new Handler();
     TextView textView;
+    String history = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,7 +70,6 @@ public class MainActivity extends AppCompatActivity {
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.RECEIVE_SMS, Manifest.permission.SEND_SMS},
                 SMS_PERMISSION_REQUEST_CODE);
-        Log.d("tag", "the request was sent");
     }
 
     @Override
@@ -88,8 +95,29 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private void registerSmsReceiver() {
+    public static String chatGPT(String text) throws Exception {
+        String url = "https://api.openai.com/v1/completions";
+        HttpURLConnection con = (HttpURLConnection) new URL(url).openConnection();
 
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json");
+        con.setRequestProperty("Authorization", "Bearer " + APIKey.API_KEY);
+
+        JSONObject data = new JSONObject();
+        data.put("model", "gpt-3.5-turbo");
+        data.put("prompt", text);
+        data.put("max_tokens", 4000);
+        data.put("temperature", 1.0);
+
+        con.setDoOutput(true);
+        con.getOutputStream().write(data.toString().getBytes());
+
+        String output = new BufferedReader(new InputStreamReader(con.getInputStream())).lines().reduce((a, b) -> a + b).get();
+
+        return new JSONObject(output).getJSONArray("choices").getJSONObject(0).getString("text");
+    }
+
+    private void registerSmsReceiver() {
 
         br = new BroadcastReceiver() {
             @Override
@@ -100,21 +128,20 @@ public class MainActivity extends AppCompatActivity {
                     for (Object pdu : pdus) {
                         SmsMessage message = SmsMessage.createFromPdu((byte[]) pdu);
                         String messageBody = message.getDisplayMessageBody();
+                        Log.d("TAG", messageBody);
                     }
                 }
 
-
-                // Create an asynch thread to
-
+                history += intent.getStringExtra("sms");
 
                 // Respond after a few seconds
                 handler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
-                        SmsManager smsManager = SmsManager.getDefault();
-                        smsManager.sendTextMessage(phoneNumber, null, "Hey low", null, null);
+                        SendMessageTask sendMessageTask = new SendMessageTask();
+                        sendMessageTask.execute();
                     }
-                }, 1000);
+                }, 100);
 
 
             }
@@ -123,6 +150,23 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    private class SendMessageTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            // Perform your background task, such as sending the message
+            SmsManager smsManager = SmsManager.getDefault();
+            try {
+                String response = chatGPT("Pretend you are in a text conversation and the conversation went like this '" + history + "' respond to the conversation appropriately in text message style");
+                history += response;
+                smsManager.sendTextMessage(phoneNumber, null, response, null, null);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            return null;
+        }
+    }
+
 }
 // TODO:
-// ChatGPT integration
+// ChatGPT integration or hardcoded AI
